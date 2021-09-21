a173fd58f29fc23d323136f34bbbabd20057c690
Egor Sozonov <Egor.Sozonov.09@gmail.com>
2021-09-21 09:45:01 +0300

    .

--- src/backend/src/api/GetApi.go
+++ src/backend/src/api/GetApi.go
@@ -1,14 +1,33 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
+type foo struct {
+	X int
+	Y string
+	Z bool
+}
+
 func HwHandlerParam(w http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
 	//w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Hello, world %v!\n", vars["id"])
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		id = 0
+	}
+	res := foo{id, "hw", true}
+	fmt.Printf("i = %d", id)
+	if err != nil {
+		fmt.Fprintf(w, "Error encoding the JSON")
+	} else {
+		json.NewEncoder(w).Encode(res)
+	}
+
 }
