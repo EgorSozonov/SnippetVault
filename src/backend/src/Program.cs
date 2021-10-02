eddd126e8b01608c1ed41bb135ad71cde85ba0b8
Egor Sozonov <Egor.Sozonov.09@gmail.com>
2021-10-03 01:05:57 +0300

    .

--- src/backend/src/Program.cs
+++ src/backend/src/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+namespace SnippetVault {
 
-namespace SnippetVault
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Hello World!");
+         CreateWebHostBuilder(args).Build().Run();
     }
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+}
 }
