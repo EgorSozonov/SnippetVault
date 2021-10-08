322ef2ce7247f453560fd663647e9dfcc0badf1e
Egor Sozonov <Egor.Sozonov.09@gmail.com>
2021-10-08 09:54:30 +0300

    .

--- src/backend/src/DTO/GetDTO.cs
+++ src/backend/src/DTO/GetDTO.cs
@@ -3,33 +3,33 @@ namespace SnippetVault {
     
 
 public class AlternativeDTO {
-	public int primaryId;
-	public string primaryCode;
-	public int primaryScore;
-	public int alternativeId;
-	public string alternativeCode;
-	public int alternativeScore;
-	public DateTime tsUpload;
+    public int primaryId;
+    public string primaryCode;
+    public int primaryScore;
+    public int alternativeId;
+    public string alternativeCode;
+    public int alternativeScore;
+    public DateTime tsUpload;
 }
 
 public class CommentDTO {
-	public int id;
-	public string author;
-	public DateTime ts;
-	public string content;
+    public int id;
+    public string author;
+    public DateTime ts;
+    public string content;
 }
 
 public class LanguageDTO {
     public int id;
-	public string name;
-	public string languageGroup;
+    public string name;
+    public string languageGroup;
 }
 
 public class ProposalDTO {
     public int proposalId;
-	public string proposalCode;
+    public string proposalCode;
     public string taskName;
-	public string languageName;
+    public string languageName;
     public DateTime tsUpload;
 }
 
@@ -37,20 +37,20 @@ public class SnippetDTO {
     public int leftId;
     public string leftCode;
     public int taskId;
-	public string taskName;
+    public string taskName;
     public int rightId;
     public string rightCode;
 }
 
 public class TaskDTO {
     public int id;
-	public string name;
-	public string description;
+    public string name;
+    public string description;
 }
 
 public class TaskGroupDTO {
     public int id;
-	public string name;
+    public string name;
 }
 
 }
