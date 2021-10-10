bdf4a0efe9e0bc735a8a95175ec303e2e903c482
Egor Sozonov <Egor.Sozonov.09@gmail.com>
2021-10-10 15:03:54 +0300

    .

--- src/backend/src/DBAccess/DeserializerMy.cs
+++ src/backend/src/DBAccess/DeserializerMy.cs
@@ -1,93 +1,73 @@
 namespace SnippetVault {
 using System;
 using System.Collections.Generic;
+using Npgsql;
 
 
 public class DBDeserializer<T> where T : class, new() {
-    Dictionary<string, Action<T, int>> settersInt;
-    Dictionary<string, Action<T, double>> settersDouble;
-    Dictionary<string, Action<T, decimal>> settersDecimal;
-    Dictionary<string, Action<T, string>> settersString;
-    Dictionary<string, Pair<int, string>> dictProperties;
-    bool isOK;
+    PropTarget[] columnTargets;
+    Action<T, int>[] settersInt;
+    Action<T, double>[] settersDouble;
+    Action<T, decimal>[] settersDecimal;
+    Action<T, string>[] settersString;
+    public bool isOK;
 
+    private struct PropTarget {
+        public ValueType propType;
+        public int indexSetter;
+    }
 
     public DBDeserializer(string[] queryColumns){
-        var emptyResult = new List<T>();
-
-        determineTypeProperties(out ValueType[] propTypes,
+        this.isOK = true;
+        determineTypeProperties(out columnTargets,
            out settersInt,
            out settersDouble,
            out settersDecimal,
            out settersString,
-           out Dictionary<string, Pair<int, string>> dictProperties,
            out string errMsg);
-        if (errMsg != "") isOK = false;
-
-        var colNums = determineColumns(queryColumns, dictCols, out string[] propNames, out errMsg);
-        if (errMsg != "") return emptyResult;
-
-    }
-
-    public T readRow(int[] colNums, Dictionary<string, Pair<int, string>> dictCols, string[] propNames, ValueType[] propTypes,
-                                    Dictionary<string, Action<T, int>> settersInt,
-                                    Dictionary<string, Action<T, double>> settersDouble,
-                                    Dictionary<string, Action<T, decimal>> settersDecimal,
-                                    Dictionary<string, Action<T, string>> settersString, out string errMsg) {
-        return null;
-    }
-
-      
-    /// <summary>
-    /// Импорт из Экселя с заданными номерами колонок, без заголовков
-    /// </summary>
-    public IList<T> importColumnNumbers(ExcelWorkbook wb, int[] colNums, string[] propNames,
-                                       ExcelImportConfig config, Func<T, bool> validation,
-                                       out string errMsg){
-        var emptyResult = new List<T>();
-      
-        ExcelWorksheet sh = determineSheet(wb, config, out errMsg);
-        if (errMsg != "") return emptyResult;
-
-        determineTypeProperties(out ValueType[] propTypes,
-           out Dictionary<string, Action<T, int>> settersInt,
-           out Dictionary<string, Action<T, double>> settersDouble,
-           out Dictionary<string, Action<T, decimal>> settersDecimal,
-           out Dictionary<string, Action<T, string>> settersString,
-           out Dictionary<string, Pair<int, string>> dictCols,
-           out errMsg);
-        if (errMsg != "") return emptyResult;
-      
-        errMsg = validateColNames(colNums, propNames, dictCols);
-        if (errMsg != "") return emptyResult;
-
-        return importFromSheet(sh, colNums, propNames, propTypes, settersInt, settersDouble,
-                               settersDecimal, settersString, config, validation, false, out errMsg);
-    }
-
-      
-    private string validateColNames(int[] colNums, string[] propNames,
-                                   Dictionary<string, Pair<int, string>> dictCols){
-        if (colNums == null || colNums.Length == 0 || propNames == null) return "Input error: empty list of columns!";
-        if (colNums.Length != propNames.Length) return "Input error: lengths of arrays don't match";
-        for (int i = 0; i < colNums.Length; ++i) {
-            if (colNums[i] < 0) return "Input error: negative column number";
-            if (propNames[i] == null || propNames[i] == "") return "Input error: empty property name!";
-            propNames[i] = propNames[i].Replace(" ", "").ToLower();
-            ++colNums[i];
-            if (!dictCols.ContainsKey(propNames[i])) return $"Input error: unknown column name {propNames[i]}";
+        if (errMsg != "") {
+            isOK = false;
+            return;
         }
-        return "";
+
+        var colNums = determineColumns(queryColumns, queryColumns, dictProperties, out errMsg);
+        if (errMsg != "") {
+            isOK = false;
+            return;
+        }
+
     }
+
+    public List<T> readResults(Dictionary<string, Pair<int, string>> dictCols, NpgsqlDataReader reader, out string errMsg) {
+        var result = new List<T>(10);
+        while (reader.Read()) {
+            var newVal = new T();
+            foreach (var pair in dictCols.Values) {
+                int j = pair.fst;
+                string propName = pair.snd;
+                if (propTypes[j] == ValueType.doubl) {                    
+                    settersDouble[propName](newVal, reader.GetDouble(j)); 
+                } if (propTypes[j] == ValueType.deciml) {
+                    settersDecimal[propName](newVal, reader.GetDecimal(j));                        
+                } else if (propTypes[j] == ValueType.integr) {                    
+                    settersInt[propName](newVal, reader.GetInt32(j));                        
+                } else if (propTypes[j] == ValueType.strin) {
+                    settersString[propName](newVal, reader.GetString(j));
+                }                 
+            }
+            result.Add(newVal);
+        }
+        errMsg = "";
+        return result;
+    }
+
       
-      
-    private void determineTypeProperties(out ValueType[] propTypes,
-           out Dictionary<string, Action<T, int>> settersInt,
-           out Dictionary<string, Action<T, double>> settersDouble,
-           out Dictionary<string, Action<T, decimal>> settersDecimal,
-           out Dictionary<string, Action<T, string>> settersString,
-           out Dictionary<string, Pair<int, string>> dictCols,
-           out string errMsg){
+    private void determineTypeProperties(out PropTarget[] columnTargets,
+            out Action<T, int>[] settersInt,
+            out Action<T, double>[] settersDouble,
+            out Action<T, decimal>[] settersDecimal,
+            out Action<T, string>[] settersString,
+            out string errMsg){
         var properties = typeof(T).GetProperties();
         propTypes = new ValueType[properties.Length];
         int i = 0;
@@ -129,7 +109,7 @@ public class DBDeserializer<T> where T : class, new() {
                                  (Action<T, string>) Delegate.CreateDelegate(typeof(Action<T, string>), null,
                                  typeof(T).GetProperty(prop.Name).GetSetMethod()));
             } else {
-                errMsg = "Unsupported column type, only Int, Double, Decimal and String columns are supported!";
+                errMsg = "Unsupported column type; only Int, Double, Decimal and String columns are supported!";
                 return;
             }
             ++i;
@@ -140,7 +120,7 @@ public class DBDeserializer<T> where T : class, new() {
       
 
 
-    private int[] determineColumns(string[] columns, Dictionary<string, Pair<int, string>> dictCols, 
+    private int[] determineColumns(string[] columns, Dictionary<string, Pair<int, string>> dictProperties, 
                                   out string errMsg){
         propNames = new string[0];
         var empty2 = new int[0];
@@ -188,78 +168,6 @@ public class DBDeserializer<T> where T : class, new() {
        return result;
    }
 
-
-    private IList<T> importFromSheet(int[] colNums, string[] propNames, ValueType[] propTypes,
-                                    Dictionary<string, Action<T, int>> settersInt,
-                                    Dictionary<string, Action<T, double>> settersDouble,
-                                    Dictionary<string, Action<T, decimal>> settersDecimal,
-                                    Dictionary<string, Action<T, string>> settersString,
-                                    out string errMsg){
-        var result = new List<T>();
-        var emptyResult = new List<T>();
-
-        int endRow = sh.Dimension.End.Row;
-        int endCol = sh.Dimension.End.Column;
-        if (config.rowNumber >= endRow || config.colNumber >= endCol) {
-            errMsg = $"Cannot start from cell ({config.rowNumber}, {config.colNumber}) as it's beyond the end of the sheet";
-            return result;
-        }
-        bool needToValidateRow = validation != null;
-        int startRow = config.rowNumber + (skipFirstRow ? 1 : 0);
-
-        for (int i = startRow; i <= endRow; ++i) {
-            var newVal = new T();
-            int foundProps = 0;
-            for (int j = 0; j < colNums.Length; ++j) {
-                var cellValue = sh.Cells[i, colNums[j]].Value;
-                if (cellValue == null) break;
- 
-                string propName = propNames[j];
-                if (propTypes[j] == ValueType.doubl) {
-                    if (double.TryParse(cellValue.ToString(), out double vl)) {
-                        settersDouble[propName](newVal, vl);
-                        ++foundProps;
-                    } else if (config.errorIgnoring == ImportErrorIgnoring.ignoreNone) {
-                        errMsg = $"Error in cell ({i}, {j}): expected a double but found {cellValue}";
-                        return emptyResult;
-                    }
-                } if (propTypes[j] == ValueType.deciml) {
-                    if (decimal.TryParse(cellValue.ToString(), out decimal vl)) {
-                        settersDecimal[propName](newVal, vl);
-                        ++foundProps;
-                    } else if (config.errorIgnoring == ImportErrorIgnoring.ignoreNone) {
-                        errMsg = $"Error in cell ({i}, {j}): expected a decimal but found {cellValue}";
-                        return emptyResult;
-                    }
-                } else if (propTypes[j] == ValueType.integr) {
-                    if (double.TryParse(cellValue.ToString(), out double vl)) {
-                        settersInt[propName](newVal, (int)vl);
-                        ++foundProps;
-                    } else if (config.errorIgnoring == ImportErrorIgnoring.ignoreNone) {
-                        errMsg = $"Error in cell ({i}, {j}): expected an integer but found {cellValue}";
-                        return emptyResult;
-                    }
-                } else if (propTypes[j] == ValueType.strin) {
-                    settersString[propName](newVal, cellValue.ToString());
-                    ++foundProps;
-                }
-            }
-
-            if (foundProps == propNames.Length) {
-                if (needToValidateRow) {
-                    var validationResult = validation(newVal);
-                    if (!validationResult) {
-                        errMsg = $"Validation failed for row {i}!";
-                        return emptyResult;
-                    }
-                }
-                result.Add(newVal);
-            }
-        }
-
-        errMsg = "";
-        return result;
-    }
 
 
     private class Pair<T, U> {
