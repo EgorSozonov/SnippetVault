211046af14e3bc9b004cc48fde7271cf6a7baf42
Egor Sozonov <Egor.Sozonov.09@gmail.com>
2021-10-23 01:58:28 +0300

    .

--- src/backend/src/API/API.cs
+++ src/backend/src/API/API.cs
@@ -1,7 +1,81 @@
 namespace SnippetVault {
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.AspNetCore.Routing;
 
-public class API {
-    public static void configure(Microsoft.AspNetCore.Routing.IEndpointRouteBuilder builder) {
+
+public static class API {
+    private static async Task snippet(HttpContext context, IDBContext dbContext) {
+        string lang1Str = context.Request.Query["lang1"][0];
+        string lang2Str = context.Request.Query["lang2"][0];
+        string tgStr = context.Request.Query["taskGroup"][0];
+        if (!int.TryParse(lang1Str, out int lang1)) return;
+        if (!int.TryParse(lang2Str, out int lang2)) return;
+        if (!int.TryParse(tgStr, out int taskGroup)) return;
+
+        await using (var cmd = new NpgsqlCommand(dbContext.getQueries.snippet, dbContext.conn)) { 
+            cmd.Parameters.AddWithValue("l1", NpgsqlTypes.NpgsqlDbType.Integer, lang1);
+            cmd.Parameters.AddWithValue("l2", NpgsqlTypes.NpgsqlDbType.Integer, lang2);
+            cmd.Parameters.AddWithValue("tg", NpgsqlTypes.NpgsqlDbType.Integer, taskGroup);
+            cmd.Prepare();
+            await using (var reader = await cmd.ExecuteReaderAsync()) {
+                //cmd.Prepare();
+                try {
+                    string[] columnNames = new string[reader.FieldCount];
+                    for (int i = 0; i < reader.FieldCount; ++i) {
+                        columnNames[i] = reader.GetName(i);
+                    }
+                    var readerSnippet = new DBDeserializer<SnippetDTO>(columnNames);
+                    if (!readerSnippet.isOK) {
+                        await context.Response.WriteAsync("Error");
+                        return;
+                    }
+
+                    var results = readerSnippet.readResults(reader, out string errMsg);
+                    if (errMsg != "") {
+                        await context.Response.WriteAsync(errMsg);
+                        return;
+                    }
+                    await context.Response.WriteAsJsonAsync<List<SnippetDTO>>(results);
+                    return;
+                } catch (Exception e) {
+                    Console.WriteLine(e.Message);
+                    await context.Response.WriteAsync("Exception");
+                    return;
+                }
+            }
+        }
+    }
+
+
+    private static RequestDelegate addDBContext(Func<HttpContext, IDBContext, Task> f, IDBContext dBContext) {
+        return x => f(x, dBContext);
+    }
+
+
+    private static void makeDelegates(IDBContext dbContext, out Tuple<RequestDelegate, string>[] delegatesForGetting, out RequestDelegate homePage) {
+        delegatesForGetting = new Tuple<RequestDelegate, string>[] {
+            new Tuple<RequestDelegate, string>(addDBContext(snippet, dbContext), "snippet")
+        };
+        homePage = context => {
+            //IFileInfo file = null;
+            return context.Response.WriteAsync("Index.html");
+        };
+    }
+
+
+    public static void configure(IEndpointRouteBuilder builder, IDBContext dbContext) {
+        makeDelegates(dbContext, out var delsForGetting, out var homePage);
+        foreach (var del in delsForGetting) {
+            builder.MapGet("/api/v1/get/" + del.Item2, del.Item1);
+        }
+        builder.MapGet("/", homePage);
     }
 }
 
