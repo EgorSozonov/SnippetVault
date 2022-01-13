7be253db579b83dc9121d83c8cce92ddca77d8b2
Egor Sozonov <Egor.Sozonov.09@gmail.com>
2022-01-14 00:24:45 +0300

    Password changing dialog

--- src/frontend/core/components/profile/Profile.ts
+++ src/frontend/core/components/profile/Profile.ts
@@ -1,6 +1,6 @@
 import "./profile.css"
 import { html } from "htm/react"
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useRef, useState } from "react"
 import IClient from "../../../ports/IClient"
 import MainState from "../../mobX/MainState"
 import { StoreContext } from "../../App"
@@ -8,16 +8,26 @@ import { fetchFromClient } from "../../utils/Client"
 import { SignInSuccessDTO } from "../../types/dto/AuthDTO"
 import { observer } from "mobx-react-lite"
 import Login from "../../commonComponents/login/Login"
+import { fmtDt } from "../../utils/DateUtils"
 
 
 const Profile: React.FunctionComponent = observer(() => {
     const state = useContext<MainState>(StoreContext)
     const client: IClient = state.app.client
+    const [changePwMode, setChangePwMode] = useState(false)
 
     const profile = state.user.profile
     
     const signOutHandler = () => {
         state.user.signOut()
+    }
+
+    const openChangePwHandler = () => {
+        setChangePwMode(true)
+    }
+
+    const closeChangePwHandler = () => {
+        setChangePwMode(false)
     }
 
     useEffect(() => {        
@@ -27,10 +37,12 @@ const Profile: React.FunctionComponent = observer(() => {
         
     }, [state.user.acc])
 
+    const oldPwRef = useRef<HTMLInputElement>(null)
+    const newPwRef = useRef<HTMLInputElement>(null)
     return html`
         <div class="profileBackground">
             <div class="profileContainer">
-                ${profile !== null 
+                ${(profile !== null && changePwMode === false)
                     ? html`
                         <div class="profileHeader">
                             <div>
@@ -39,16 +51,26 @@ const Profile: React.FunctionComponent = observer(() => {
                             <div class="profileHeaderSubscript">
                                 <h5>User Profile</h5>
                             </div>
+                            <div class="profileChangePasswordButton clickable" title="Change password" onClick=${openChangePwHandler}>Pw
+                            </div>
                         </div>
-                        <div>Registration date: </div>
+                        <div>Registration date: ${fmtDt(profile.tsJoined)}</div>
                         <div>Total number of proposals: ${profile.proposalCount}</div>
                         <div>Approved proposals: ${profile.approvedCount}</div>
                         <div>Approved proposals selected as primary: ${profile.primaryCount}</div>
                         <div onClick=${signOutHandler} class="profileSignOut">Sign out</div>                
                     `
-                    : html `
-                        <${Login} />                    
-                    `
+                    : (changePwMode === true 
+                        ? html`
+                            <div>
+                                <div>Old password:</div>
+                                <div><input type="password" ref=${oldPwRef} /></div>
+                                <div>New password:</div>
+                                <div><input type="password" ref=${newPwRef} /></div>
+                                <div class="clickable" onClick=${closeChangePwHandler}>Cancel</div>
+                            </div>
+                        ` 
+                        : html `<${Login} />`)
                 }
             </div>
         </div>
