8db8725d27cb0be650fa5c35bc4dc8969972695e
Egor Sozonov <Egor.Sozonov.09@gmail.com>
2022-05-23 21:09:36 +0300

    DNS dep fix + DB configuration

--- src/backend/main/tech/sozonov/SnippetVault/cmn/config/DBConfiguration.java
+++ src/backend/main/tech/sozonov/SnippetVault/cmn/config/DBConfiguration.java
@@ -1,26 +1,30 @@
 package tech.sozonov.SnippetVault.cmn.config;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import io.r2dbc.spi.ConnectionFactory;
 import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
 import io.r2dbc.postgresql.PostgresqlConnectionFactory;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
-import org.springframework.r2dbc.core.DatabaseClient;
 
 @Configuration
 public class DBConfiguration extends AbstractR2dbcConfiguration {
 
 
+@Autowired
+private Environment env;
 @Bean
 public ConnectionFactory connectionFactory() {
-
     return new PostgresqlConnectionFactory(
             PostgresqlConnectionConfiguration.builder()
-                    .host("localhost")
-                    .database("test")
-                    .username("user")
-                    .password("password")
+                    .host(env.getProperty("db.host"))
+                    .database(env.getProperty("db.dbname"))
+                    .schema(env.getProperty("db.schema"))
+                    .username(env.getProperty("spring.r2dbc.username"))
+                    .password(env.getProperty("spring.r2dbc.password"))
+                    .port(Integer.parseInt(env.getProperty("db.port")))
                     .build()
     );
 }
